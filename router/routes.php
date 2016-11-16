f3462ff3fd60f11932a1163e0d956e3da42cfaa9
wacxt <980997354@qq.com>
2016-11-16 10:16:45 +0800

    0.0.1

--- router/routes.php
+++ router/routes.php
@@ -3,12 +3,9 @@
 $klein = new \Klein\Klein();
 
 $klein->respond('GET','/',function(){
-	//$Home = new HomeController();
-	//$Home->home(); 
-	session('my','123');
-	echo session('my');
-	session('my',null);
-	echo session('my');
+	$Home = new HomeController();
+	$Home->home(); 
+
 });
 
 $klein->dispatch();
